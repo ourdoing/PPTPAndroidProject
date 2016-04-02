d0b1ba8a454e73fa9abd0903ffff97db411c9580
BlindingDark <844620174@qq.com>
2016-04-02 15:24:36 +0800

    ver1.1新界面

--- app/src/main/java/com/ourdoing/pptp/activity/MainActivity.java
+++ app/src/main/java/com/ourdoing/pptp/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ourdoing.pptp.activity;
 
+import com.ourdoing.pptp.R;
 import com.xys.libzxing.zxing.activity.*;
 
 import android.content.Intent;
@@ -10,12 +11,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.ourdoing.pptp.R;
 import com.ourdoing.pptp.control.PageController;
 
 
 public class MainActivity extends AppCompatActivity {
-    private EditText ip_text;
+    //private EditText ip_text;
+    private String iPAndPort = "";
     private PageController pageController = new PageController();
 
     @Override
@@ -25,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads().detectDiskWrites().detectNetwork().penaltyLog().build());
         StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects().detectLeakedClosableObjects().penaltyLog().penaltyDeath().build());
 
-        ip_text = (EditText) findViewById(R.id.ipAddr);
+        //ip_text = (EditText) findViewById(R.id.ipAddr);
     }
 
     public String getIPAndPort() {
-        return ip_text.getText().toString();
+        //return ip_text.getText().toString();
+        return iPAndPort;
     }
 
     public void sendNext(View view) {
@@ -65,7 +67,16 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             String result = data.getExtras().getString("result");
-            ip_text.setText(result);
+            //ip_text.setText(result);
+            this.iPAndPort = result;
         }
+    }
+
+    public void beerClick(View view) {//熊前
+        sendPre(view);
+    }
+
+    public void fishClick(View view) {//鱼后
+        sendNext(view);
     }
 }
